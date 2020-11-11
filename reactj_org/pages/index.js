c5ae4a10eae4c9d01ddcd18442f8f9fa0064eff3
Pedro Ottoni Carneiro <pedrottoni@gmail.com>
2020-11-11 20:01:00 -0300

    Super ideia megacriativa do dia 8

--- reactj_org/pages/index.js
+++ reactj_org/pages/index.js
@@ -1,43 +1,46 @@
 import { useState } from "react";
 
 function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-            <CapsLock texto="Me deixe em caixa alta, por favor" />
-            <CapsLockII>Me deixe em caixa alta também, por favor</CapsLockII>
-            <Contador />
-        </div>
-    )
+  return (
+    <div>
+      <h1>Gatoooona</h1>
+      <CapsLock texto="Eu esqueci do nosso dia esse mês" />
+      <CapsLockII>Só que um textinho na minha agenda me fez lembrar</CapsLockII>
+      <h3>
+      <CapsLockII>E o sorriso</CapsLockII> no meu rosto que se abriu.. agora não quer mais fechar
+      </h3>
+      <Contador />
+    </div>
+  );
 }
 
 function CapsLock(props) {
-    const textoInserido = props.texto;
-    const textoEmCaixaAlta = textoInserido.toUpperCase();
+  const textoInserido = props.texto;
+  const textoEmCaixaAlta = textoInserido.toUpperCase();
 
-    return <div>{textoEmCaixaAlta}</div>;
-};
+  return <div>{textoEmCaixaAlta}</div>;
+}
 
 function CapsLockII(props) {
-    const textoInserido = props.children;
-    const textoEmCaixaAlta = textoInserido.toUpperCase();
+  const textoInserido = props.children;
+  const textoEmCaixaAlta = textoInserido.toUpperCase();
 
-    return <div>{textoEmCaixaAlta}</div>;
-};
+  return <div>{textoEmCaixaAlta}</div>;
+}
 
 function Contador() {
-    const [contador, setContador] = useState(0);
+  const [contador, setContador] = useState(10);
 
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
+  function adicionarContador() {
+    setContador(contador * 2);
+  }
 
-    return (
-        <div>
-            <h2>{contador}</h2>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    );
-};
+  return (
+    <div>
+      <h2>Sua beleza é maior do que <span style={{color: 'red'}}>{contador}</span> milhões de estrelas juntas iluminando o Nilo</h2>
+      <button onClick={adicionarContador}>É mais?</button>
+    </div>
+  );
+}
 
-export default Home
+export default Home;
